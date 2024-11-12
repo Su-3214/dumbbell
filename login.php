e5b7c153b69e32b4030a7c23a2e5a40797d9ed12
MatsumotoKazuki29 <2301027@s.asojuku.ac.jp>
2024-11-12 12:42:02 +0900

    commit

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h3>THE・脳筋</h3>
+        <h3>ログイン</h3>
+        <p>会員の方は、登録時に入力されたEメールアドレスとパスワードでログインしてください。</p>
+    </div>
+    <div class="label-container">
+    メールアドレス<br>
+    </div>
+    <div class="label-container2">
+        <input type="text" name="mail" class="textbox"><br>
+    </div>
+    <div class="label-container">
+    パスワード<br>
+    </div>
+    <div class="label-cotainer2">
+        <input type="text" name="pass" class="textbox"><br>
+    </div>
+</body>
+</html>
