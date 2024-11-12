793682fa6e1c794a6128b499b38888e5fc2d40ab
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-12 12:42:55 +0900

    commit

--- kensaku.php
+++ kensaku.php
@@ -7,11 +7,35 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="close-container">
-            <input type="image" src="./img/close.png" alt="画像ボタン" width="50" height="50">
-            <span>閉じる</span>
+    <div class="container2">
+
+        <div class="close-button">
+            <input type="button" type="submit" value="✕">
         </div>
+
+        <div class="flex-container2">
+            　　　キーワード
+            <div class="right-align">
+                <input type="text" name="keyword">
+            </div>
+        </div>
+
+        <div class="flex-container2">
+            　　　　価格帯
+            <div class="right-align">
+                <input type="text" name="min" class="min">　～　<input type="text" name="max" class="max">
+            </div>
+        </div>
+
+        <div class="flex-container2">
+            　　　　　味
+            <div class="right-align">
+                <input type="text" name="taste">
+            </div>
+        </div>
+
+
+
     </div>
 </body>
 </html>
