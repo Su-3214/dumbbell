2211bbf1977049f2c31a416d28be3f77ef994655
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-18 13:49:10 +0900

    commit

--- kensakukekka.php
+++ kensakukekka.php
@@ -10,13 +10,13 @@
     <div class="container">
         <div class="header">
             THE・脳筋
-                <input type="button" type="submit" value="購入履歴">
-                <input type="button" type="submit" value="ログアウト">
-                <input type="image" src="./img/button.png" alt="画像ボタン" 
-                width="30" height="30" >
-                <input type="image" src="./img/cart.png" alt="画像ボタン" 
-                width="30" height="30" >
-            </div>
+            <input type="button" type="submit" value="購入履歴">
+            <input type="button" type="submit" value="ログアウト">
+            <input type="image" src="./img/button.png" alt="画像ボタン" width="30" height="30">
+            <input type="image" src="./img/cart.png" alt="画像ボタン" width="30" height="30">
+        </div>
+        <h3>検索結果</h3>
+        <br>
     </div>
 </body>
 </html>
