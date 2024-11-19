9ee62405dc234b826dd2456d54c9f60aa92f72b3
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-19 12:15:14 +0900

    commit

--- kounyuurireki.php
+++ kounyuurireki.php
@@ -7,9 +7,28 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="kounyuurireki">
-        <h2>購入履歴</h2>
-        <button type="submit" name="top">トップページ</button>
+    <div class="container">
+        <div class="kounyuurireki">
+            <div class="krireki">購入履歴</div>
+            <button type="submit" name="top" class="toppage">トップページ</button>
+        </div>
+
+        <hr class="dotted-line">
+        <div class="item">
+            <img src="./img/chocolate.png" alt="画像" width="75px" height="100px">
+            <div class="details">
+                <div>【チョコレート味３㎏】</div>
+                <div class="price">6,990円</div>
+            </div>
+        </div>
+        <hr class="dotted-line">
+
+
+
+
+
+
+
     </div>
 </body>
 </html>
---
+++ tourokukanryou.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会員登録完了</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h3>THE・脳筋</h3>
+        <br><br><br><br><br>
+        <br><br><br>
+        <h2>
+            会員登録が<br>
+            完了しました
+        </h2>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+
+        <button class="tologin" type="button">ログイン画面へ</button>
+    </div>
+</body>
+</html>
