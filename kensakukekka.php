bff61e84cadb27eebeaad1c61b6da420fa9bce9f
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-19 11:47:49 +0900

    commit

--- kensakukekka.php
+++ kensakukekka.php
@@ -17,7 +17,33 @@
                 <input type="image" src="./img/cart.png" alt="画像ボタン" 
                 width="30" height="30" >
             </div>
-        </div>
-    </div>
+
+            <div class="kekka">
+                <h2>検索結果</h2>
+            </div>
+
+            <div class="youso">
+                <div>キーワード：XXXXX</div>
+                <div>価格帯：XXXXX～XXXXX</div>
+            </div>
+            <div class="azi">味：XXXXXX</div>
+
+            <div class="kensuu">X件</div>
+
+            <div class="shouhin-container">
+                <div class="shouhin">
+                    <img src="./img/chocolate.png" alt="画像" width="117px" height="135px">
+                    <div>【チョコレート味３㎏】</div>
+                    <div class="price">6,990円</div>
+                    <button class="cartbutton" type="button">カートに入れる</button>
+                </div>
+            </div>
+
+            <div class="page">
+                <h2>X件</h2>
+            </div>
+
+            <button class="back" type="button">戻る</button>
+
 </body>
 </html>
