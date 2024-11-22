ec747e64de0a3b3eee2675e1686972762f1f9beb
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-22 13:39:14 +0900

    c

--- Management/kanrisyalogin.php
+++ Management/kanrisyalogin.php
@@ -3,24 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>管理者登録</title>
+    <title>管理者ログイン</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <div class="container">
         <h3>THE・脳筋</h3>
-        <h3>新規会員登録</h3>
+        <h3>ログイン</h3>
+        <p class="loginmessage">
+            管理者の方は、登録時に入力されたEメールアドレスと<br>
+            パスワードでログインしてください。
+        </p>
         <br>
-        <div class="label-container">
-            お名前
-        </div>
-        <div class="label-container3">
-            <input type="text" name="name" class="textbox"><br>
-        <div class="note">
-            ※苗字と名前の間にスペースは不要です<br>
-        </div>
-        </div>
-
         <div class="label-container">
         メールアドレス<br>
         </div>
@@ -35,7 +29,7 @@
             <input type="text" name="pass" class="textbox"><br>
         </div>
 
-        <button class="touroku" type="button">管理者登録</button>
+        <button class="login" type="button">ログイン</button>
     </div>
 </body>
 </html>
---
+++ Management/tourokukanryou.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者登録完了</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h3>THE・脳筋</h3>
+        <br><br><br><br><br>
+        <br><br><br>
+        <h1>
+            登録完了
+        </h1>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+
+        <button class="tologin" type="button">ログイン画面へ</button>
+    </div>
+</body>
+</html>
