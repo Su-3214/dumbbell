106a75a8b2b1015aad11776df0cc4a6703b8df8b
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-22 14:29:01 +0900

    c

---
+++ Management/kaiinhenkou.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container3">
+        <div class="header">
+            THE・脳筋　　　　　管理者　　　　　
+                <input type="button" type="submit" value="ログアウト">
+        </div>
+    
+        会員ID：XXXXX<br><br>
+
+        会員ID：XXXXX<br><br>
+
+        メール：XXXXX@XXXXX<br><br>
+
+        <hr class="dotted-line">
+
+        <div class="label-container5">
+            <label for="kaiinid">会員ID</label>
+            <input type="text" name="kaiinid" class="skid">
+        </div>
+        <div class="label-container5">
+            <label for="kaiinmei">会員名</label>
+            <input type="text" name="kaiinname" class="skid">
+        </div>
+        <div class="label-container5">
+            <label for="mail">メール</label>
+            <input type="text" name="mail" class="skid">
+        </div>
+        <div class="label-container5">
+            <label for="Pass" class="Pass">Pass</label>
+            <input type="text" name="pass" class="skid">
+        </div>
+        <div class="label-container5">
+            <label for="jusyo" class="jusyo">住所</label>
+            <input type="text" name="jusyo" class="skid">
+        </div>
+
+
+    </div>
+</body>
+</html>
---
+++ Management/kanrisya_kaiin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container3">
+        <div class="header">
+            THE・脳筋　　　　　管理者　　　　　
+                <input type="button" type="submit" value="ログアウト">
+        </div>
+        <h3>会員一覧</h3>
+        会員ID　
+
+        会員名　
+        
+        メールアドレス　
+        
+        Pass　
+
+        住所　
+            
+    </div>
+</body>
+</html>
