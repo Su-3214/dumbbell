77054819cbf385cc61933c3c8589cfb2ca5380a0
Su-3214 <2301026@s.asojuku.ac.jp>
2024-11-22 11:35:59 +0900

    ファイル移動

--- shinkitouroku.php
+++ shinkitouroku.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新規会員登録</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <h3>THE・脳筋</h3>
@@ -15,34 +17,38 @@
         </div>
         <div class="label-container3">
             <input type="text" name="name" class="textbox"><br>
-        <div class="note">
-            ※苗字と名前の間にスペースは不要です<br>
+            <div class="note">
+                ※苗字と名前の間にスペースは不要です<br>
+            </div>
         </div>
-        </div>
-
-        <div class="label-container">
-        メールアドレス<br>
-        </div>
-        <div class="label-container2">
+        <!--ここからDB接続を行う-->
+        <form action="register.php" method="POST">
+            <div class="label-container">
+                メールアドレス<br>
+            </div>
+            <div class="label-container2">
                 <input type="text" name="mail" class="textbox"><br>
-        </div>
+            </div>
 
-        <div class="label-container">
-        パスワード<br>
-        </div>
-        <div class="label-container2">
-            <input type="text" name="pass" class="textbox"><br>
-        </div>
+            <div class="label-container">
+                パスワード<br>
+            </div>
+            <div class="label-container2">
+                <input type="text" name="pass" class="textbox"><br>
+            </div>
 
-        <div class="label-container">
-        住所<br>
-        </div>
-        <div class="label-container4">
-            <input type="text" name="adress" class="textbox2"><br><br>
-        </div>
+            <div class="label-container">
+                住所<br>
+            </div>
+            <div class="label-container4">
+                <input type="text" name="adress" class="textbox2"><br><br>
+            </div>
 
-        <button class="back" type="button">戻る</button>
-        <button class="touroku" type="button">この内容で登録する</button>
+            <button class="back" type="button">戻る</button>
+            <button class="touroku" type="button">この内容で登録する</button>
+        </form>
+
     </div>
 </body>
+
 </html>
