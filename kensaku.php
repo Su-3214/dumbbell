50c8c49b0c8a5007c0e3658636529b153f6315ef
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-22 11:50:17 +0900

    commit

--- kensaku.php
+++ kensaku.php
@@ -13,8 +13,21 @@
                 <button type="submit" name="batu"><b>×</b></button>
             </div>
             <span><b>閉じる</b></span>
+        </div><br><br><br><br><br><br>
+        <div class="youso">
+            <div class="key"><b>キーワード：</b></div>
+            <input type="textbox" name="keyword" class="keyword"><br>
         </div>
-        
+        <div class="youso">
+            <div class="kakakutai"><b>価格帯：</b></div>
+            <input type="textbox" name="max" class="max">
+            <div class="kara">～</div><input type="textbox" name="min" class="min"><br>
+        </div>
+        <div class="youso">
+            <div class="mi"><b>味：</b></div>
+            <input type="text" name="taste" class="taste">
+        </div>
+        <button class="search" type="button">検索</button>
     </div>
 </body>
 </html>
