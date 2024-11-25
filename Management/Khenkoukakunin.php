c73c319d5e18ee1c8889f00da723154316f0f2dc
Yamaguchi0412 <2401113@s.asojuku.ac.jp>
2024-11-25 09:40:39 +0900

    c

---
+++ Management/Khenkoukakunin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品変更</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container3">
+        <div class="header">
+            THE・脳筋　　　　　管理者　　　　　
+                <input type="button" type="submit" value="ログアウト">
+        </div>
+
+        <h3>会員情報</h3>
+
+        会員ID：XXXXX<br><br><br>
+
+        会員名：【XXXXX】<br><br><br>
+
+        メールアドレス：XXXXX@XXXXX<br><br><br>
+
+        パスワード：XXXXX<br><br><br>
+
+        住所：XX県XX市XX区XXX X-XX-XX<br><br><br>
+
+        
+    <button type="submit" name="tozi" class="tozi">閉じる</button>
+
+    </div>
+</body>
+</html>
